b9c335d55e8d5b5950ae6c7d6f77d367abafacfc
DanielC14 <danielggcarneiro7@gmail.com>
2018-07-20 11:43:43 +0100

    up

--- Server/Objects/Animal/AnimalModel.js
+++ Server/Objects/Animal/AnimalModel.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 let schema = new mongoose.Schema({
-    species: { type: String, required: true, unique: false },
+    species: { type: String, required: false, unique: false },
     name: { type: String, required: false, unique: false, default: "Não definido" },
     breed: { type: String, required: false, unique: false, default: "Não definido" },
-    gender: { type: String, required: true, unique: false },
+    gender: { type: String, required: false, unique: false },
     age: { type: String, required: false, unique: false, default: "Não definido" },
     description: { type: String, required: false, unique: false, default: "Sem descrição" },
-    type: { type: Number, required: true, unique: false },
+    type: { type: Number, required: false, unique: false },
     picture: { type: String, required: false, unique: false, default: "https://www.goodfreephotos.com/albums/vector-images/dog-cartoon-vector-clipart.png" },
     lat: { type: Number, default: null },
     lng: { type: Number, default: null },
